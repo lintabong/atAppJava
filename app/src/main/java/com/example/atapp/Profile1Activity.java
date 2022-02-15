63b74f2f9fd46aa98a27655aabb5bd43b7ed0373
Lintang Bongkar IRIS <lintang.bb@gmail.com>
2022-02-15 10:05:17 -0800

    second triall

--- app/src/main/java/com/example/atapp/Profile1Activity.java
+++ app/src/main/java/com/example/atapp/Profile1Activity.java
@@ -5,13 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Profile1Activity extends AppCompatActivity {
+
+    EditText editName;
+    EditText editAddr;
+    EditText editPass;
+    EditText editConf;
+
+    Button butSave;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile1);
+
+        editName = (EditText) findViewById(R.id.textName);
+        editAddr = (EditText) findViewById(R.id.textAddress);
+        editPass = (EditText) findViewById(R.id.textPass);
+        editConf = (EditText) findViewById(R.id.textConf);
+
+        butSave = (Button) findViewById(R.id.butSave);
+
+        butSave.setOnClickListener(v -> {
+            String getName = editName.getText().toString();
+            String getAddr = editAddr.getText().toString();
+            String getPass = editPass.getText().toString();
+            String getConf = editConf.getText().toString();
+
+
+            if (getPass.equals(getConf)){
+                myRef.child("user").child(getName).child("address").setValue(getAddr);
+            }
+        });
+
+
     }
 
     public void profileToSOS(View view) {
